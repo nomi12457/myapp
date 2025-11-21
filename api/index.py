4b638311fa6687baa348687e3c1811e2025e2e3d
Nom Virus <romanrashid772@gmail.com.com>
2025-11-21 11:16:56 +0500

    add app.py and fix imports

--- api/index.py
+++ api/index.py
@@ -1,7 +1,4 @@
-# api/index.py
-from app import app    # import your Flask app object from root app.py
-import awsgi
+from app import app as application
 
 def handler(event, context):
-    # awsgi turns the Flask WSGI app into a Lambda/APIGW-compatible response
-    return awsgi.response(app, event, context)
+    return application
